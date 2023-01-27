14cfad7d93d0766e40062578ebf5921579ef5acc
Eric Smith <esmith11235@gmail.com>
2023-01-27 18:45:11 -0500

    refactor: add Google auth and change prisma schema for planetscale initial setup

--- apps/expo/src/constants.ts
+++ apps/expo/src/constants.ts
@@ -4,7 +4,7 @@
  *
  * NOTE: we recommend putting the publishable key here instead of in your .env
  * files for two reasons:
- * 1. It's okay for this to be "public" (CLERK_SECRET_KEY 
+ * 1. It's okay for this to be "public" (CLERK_SECRET_KEY
  *    NEVER be public)
  * 2. Parsing the .env file in Metro/Expo runs the risk of including the
  *    variables above that we don't want (and it's obnoxious to do right as a
@@ -12,7 +12,8 @@
  */
 
 // FOR CLERK APPS AFTER 1/18/2023 pk_test_XXXXXXXXXXXXXXXXXXXXXXXX
-export const CLERK_PUBLISHABLE_KEY = undefined;
+export const CLERK_PUBLISHABLE_KEY =
+  "pk_test_YWxpdmUtZWxlcGhhbnQtMTIuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
 if (CLERK_PUBLISHABLE_KEY === undefined) {
   throw new Error("CLERK_PUBLISHABLE_KEY is not defined");
