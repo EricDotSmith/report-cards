d0f324dc8ff673ed42ff6ee557e9a145aad2e440
Eric Smith <esmith11235@gmail.com>
2023-01-29 17:11:12 -0500

    test: add some testing with routes

--- apps/nextjs/src/pages/test/index.tsx
+++ apps/nextjs/src/pages/test/index.tsx
@@ -1,8 +1,10 @@
 import { PageContainer } from "../../components/Page";
+import Link from "next/link";
 
 const TestPage: React.FC = () => {
   return (
     <PageContainer>
+      <Link href="/">Go home</Link>
       <div className="flex w-full flex-col items-center">
         <div className="m-2 h-32 w-32 rounded-lg bg-yellow-400 shadow-lg">
           x
