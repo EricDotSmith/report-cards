9f3d7790194f6bcfaa3ae638b11bb13b5fcf76ab
Eric Smith <esmith11235@gmail.com>
2023-01-31 21:39:46 -0500

    feat: add background color for each primary page

--- apps/nextjs/src/pages/dashboard/index.tsx
+++ apps/nextjs/src/pages/dashboard/index.tsx
@@ -5,20 +5,27 @@ import { NextPage } from "next";
 
 const Dashboard: NextPage = () => {
   return (
-    <PageContainer
-      pageBottomBar={<PageBottomBar />}
-      pageLeftBar={<PageLeftBar />}
-      pageRightBar={
-        <div className="flex h-full border-4 border-r-0 border-t-0 border-b-0 border-sky-400">
-          some helpful card
-        </div>
-      }
-      pageTopBar={
-        <div className="w-full border-b bg-white  p-4">some helpful card</div>
-      }
-    >
-      <div>Dashboard Page</div>
-    </PageContainer>
+    <>
+      <style global jsx>{`
+        body {
+          background-color: #58c1fa;
+        }
+      `}</style>
+      <PageContainer
+        pageBottomBar={<PageBottomBar />}
+        pageLeftBar={<PageLeftBar />}
+        pageRightBar={
+          <div className="flex h-full border-4 border-r-0 border-t-0 border-b-0 border-sky-400">
+            some helpful card
+          </div>
+        }
+        pageTopBar={
+          <div className="w-full border-b bg-white  p-4">some helpful card</div>
+        }
+      >
+        <div>Dashboard Page</div>
+      </PageContainer>
+    </>
   );
 };
 
--- apps/nextjs/src/pages/test/index.tsx
+++ apps/nextjs/src/pages/test/index.tsx
@@ -6,31 +6,38 @@ import { NextPage } from "next";
 
 const Test: NextPage = () => {
   return (
-    <PageContainer
-      pageBottomBar={<PageBottomBar />}
-      pageLeftBar={<PageLeftBar />}
-      pageRightBar={
-        <div className="flex h-full border-4 border-r-0 border-t-0 border-b-0 border-sky-400">
-          some helpful card
+    <>
+      <style global jsx>{`
+        body {
+          background-color: #f2aa4b;
+        }
+      `}</style>
+      <PageContainer
+        pageBottomBar={<PageBottomBar />}
+        pageLeftBar={<PageLeftBar />}
+        pageRightBar={
+          <div className="flex h-full border-4 border-r-0 border-t-0 border-b-0 border-sky-400">
+            some helpful card
+          </div>
+        }
+        pageTopBar={
+          <div className="w-full border-b bg-white  p-4">some helpful card</div>
+        }
+      >
+        <Link href="/">Go home</Link>
+        <div className="flex w-full flex-col items-center">
+          <div className="m-2 h-32 w-32 rounded-lg bg-yellow-400 shadow-lg">
+            x
+          </div>
+          <div className="m-2 h-32 w-32 rounded-lg bg-yellow-400 shadow-lg">
+            x
+          </div>{" "}
+          <div className="m-2 h-32 w-32 rounded-lg bg-yellow-400 shadow-lg">
+            x
+          </div>
         </div>
-      }
-      pageTopBar={
-        <div className="w-full border-b bg-white  p-4">some helpful card</div>
-      }
-    >
-      <Link href="/">Go home</Link>
-      <div className="flex w-full flex-col items-center">
-        <div className="m-2 h-32 w-32 rounded-lg bg-yellow-400 shadow-lg">
-          x
-        </div>
-        <div className="m-2 h-32 w-32 rounded-lg bg-yellow-400 shadow-lg">
-          x
-        </div>{" "}
-        <div className="m-2 h-32 w-32 rounded-lg bg-yellow-400 shadow-lg">
-          x
-        </div>
-      </div>
-    </PageContainer>
+      </PageContainer>
+    </>
   );
 };
 
