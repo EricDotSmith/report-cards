e8eb3064b3c5968b9b977564b2e9008cce9bb292
Eric Smith <esmith11235@gmail.com>
2023-05-30 17:48:35 -0400

    feat: add contact form and home page styles

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
 import { NextSeo } from "next-seo";
 import Image from "next/image";
+import emailjs from "@emailjs/browser";
 
 const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Price", href: "#" },
+  { name: "Product", href: "#product" },
+  { name: "Price", href: "#price" },
+  { name: "Contact", href: "#contact" },
 ];
 
 const PAGE_COLOR = "#f6f3ec";
@@ -59,9 +61,9 @@ const Home: NextPage = () => {
           >
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">Report Cards</span>
                 <Image
-                  src="/report-card-icon.png"
+                  src="/report-card-icon-dark.png"
                   width={32}
                   height={32}
                   alt="Picture of the author"
@@ -151,12 +153,17 @@ const Home: NextPage = () => {
               </div>
             </div> */}
               <div className="text-center">
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                <h1
+                  className="animate-text bg-gradient-to-r from-orange-500 via-purple-500 to-sky-500 bg-clip-text 
+            text-4xl font-semibold tracking-tight  text-gray-900 text-transparent
+            sm:text-6xl"
+                >
                   Report cards, done simply
                 </h1>
+
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Get started generating reports for your students in minutes.
-                  Saving you hours of time.
+                  Get started generating comments for your students report cards
+                  in minutes. Saving you hours of time.
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <Link
@@ -174,7 +181,7 @@ const Home: NextPage = () => {
                 </div>
               </div>
             </div>
-            <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]">
+            <div className="absolute inset-x-0 top-[calc(100%-60rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-80rem)]">
               <svg
                 className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                 viewBox="0 0 1155 678"
@@ -200,8 +207,34 @@ const Home: NextPage = () => {
                 </defs>
               </svg>
             </div>
+            <div className="absolute inset-x-0 top-[calc(100%-40rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]">
+              <svg
+                className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+7rem)] sm:h-[42.375rem]"
+                viewBox="0 0 1155 678"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="url(#0-546c-4772-8c71-4d3f06d544bc)"
+                  fillOpacity="0.7"
+                  d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+                />
+                <defs>
+                  <linearGradient
+                    id="0-546c-4772-8c71-4d3f06d544bc"
+                    x1="1155.49"
+                    x2="-78.208"
+                    y1=".177"
+                    y2="474.645"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#f08fa2" />
+                    <stop offset={1} stopColor="#b97cfc" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+            <ContactUs />
           </div>
-          <div className="h-64"></div>
         </main>
       </div>
     </>
@@ -233,6 +266,109 @@ const AuthShowcase: React.FC = () => {
           Log in <span aria-hidden="true">&rarr;</span>
         </Link>
       )}
+    </div>
+  );
+};
+
+const ContactUs: React.FC = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_56t4kb8",
+        "template_xoqwght",
+        form.current,
+        "rHykzTHgaxaq2J74_",
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+  };
+
+  return (
+    <div
+      id="contact"
+      className="relative isolate bg-transparent px-6 py-24 sm:py-32 lg:px-8"
+    >
+      <div className="mx-auto max-w-xl lg:max-w-4xl">
+        <h2 className="text-4xl font-bold tracking-tight text-sky-600">
+          Contact
+        </h2>
+        <div className="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
+          <form ref={form} onSubmit={sendEmail} className="lg:flex-auto">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="user_name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="user_name"
+                    id="user_name"
+                    autoComplete="name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="user_email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="user_email"
+                    id="user_email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={4}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    defaultValue={""}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="mt-10">
+              <button
+                type="submit"
+                value="Send"
+                className="block w-full rounded-md bg-sky-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+              >
+                Let’s talk
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- apps/nextjs/tailwind.config.cjs
+++ apps/nextjs/tailwind.config.cjs
@@ -2,4 +2,23 @@
 module.exports = {
   presets: [require("@acme/tailwind-config")],
   plugins: [require("@tailwindcss/forms")],
+  theme: {
+    extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
+  },
 };
