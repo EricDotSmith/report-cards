098ab7154672e0cb150b6a6039044276f23f8e67
Eric Smith <esmith11235@gmail.com>
2023-05-30 15:03:39 -0400

    feat: add limit to 5 classes each day that can be created

--- packages/api/src/router/class.ts
+++ packages/api/src/router/class.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { router, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
@@ -27,7 +28,31 @@ export const classRouter = router({
       },
     });
   }),
-  create: protectedProcedure.mutation(({ ctx }) => {
+  create: protectedProcedure.mutation(async ({ ctx }) => {
+    //find the most recently created class for the teacher
+    const mostRecentCreatedClassesForUser = await ctx.prisma.class.findMany({
+      where: { teacherId: ctx.user.id },
+      orderBy: { createdAt: "desc" },
+      select: { createdAt: true },
+      take: 5,
+    });
+
+    //check to see if most 5 recent classes were created in the the last day
+    const mostRecentCreatedClassesForUserInLastDay =
+      mostRecentCreatedClassesForUser.filter((classObj) => {
+        const oneDayAgo = new Date();
+        oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+        return classObj.createdAt > oneDayAgo;
+      });
+
+    //check to see if the user has created more than 5 classes in the last day
+    if (mostRecentCreatedClassesForUserInLastDay.length >= 5) {
+      throw new TRPCError({
+        code: "TOO_MANY_REQUESTS",
+        message: "Too many classes created in the last day",
+      });
+    }
+
     return ctx.prisma.class.create({
       data: {
         teacher: {
