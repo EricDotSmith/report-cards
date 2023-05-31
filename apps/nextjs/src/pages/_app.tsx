5caa94a95b66d969113ede90f6fa131076ddc7c2
Eric Smith <esmith11235@gmail.com>
2023-05-31 14:59:43 -0400

    refactor: remove onboarding modal

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -3,16 +3,13 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { trpc } from "../utils/trpc";
-import OnboardingModalWrapper from "../components/onboarding/OnboardingModalWrapper";
 import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <OnboardingModalWrapper>
-        <Component {...pageProps} />
-        <Analytics />
-      </OnboardingModalWrapper>
+      <Component {...pageProps} />
+      <Analytics />
     </ClerkProvider>
   );
 };
